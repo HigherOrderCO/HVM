10676c95a186e4bb476653a88028b3fd8606f676
Kelvin Santos <kelvinsteinersantos@gmail.com>
2022-01-04 16:28:16 -0300

    small optimization with unchecked access on `link`

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,14 @@ mod lambolt;
 use runtime as rt;
 
 fn main() {
-  //let mut worker = rt::new_worker();
-  //worker.size = 1;
-  //worker.node[0] = rt::Cal(0, 0, 0);
-  //let start = Instant::now();
-  //rt::normal(&mut worker, 0);
-  //let total = (start.elapsed().as_millis() as f64) / 1000.0;
-  //println!("* rwts: {} ({:.2}m rwt/s)", worker.cost, (worker.cost as f64) / total / 1000000.0);
-  //println!("* time: {:?}", total);
+  let mut worker = rt::new_worker();
+  worker.size = 1;
+  worker.node[0] = rt::Cal(0, 0, 0);
+  let start = Instant::now();
+  rt::normal(&mut worker, 0);
+  let total = (start.elapsed().as_millis() as f64) / 1000.0;
+  println!("* rwts: {} ({:.2}m rwt/s)", worker.cost, (worker.cost as f64) / total / 1000000.0);
+  println!("* time: {:?}", total);
 
-  println!(":pp");
+  // println!(":pp");
 }
