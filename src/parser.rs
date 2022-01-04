927ba011c892cdfb829f7cdadcb62f6d67c36e5f
MaiaVictor <srvictormaia@gmail.com>
2022-01-04 15:05:32 -0300

    Parser WIP

--- src/parser.rs
+++ src/parser.rs
@@ -13,7 +13,7 @@ struct State<'a> {
   index: usize
 }
 
-type Parser<A> = Box<dyn Fn(State) -> (State, A)>;
+type Parser<A> = Box<dyn FnOnce(State) -> (State, A)>;
 
 // TypeScript:
 //export function read<A>(parser: () => Parser<A>, code: string): A {
@@ -162,6 +162,109 @@ fn consume(c: &'static Code) -> Parser<()> {
 }
 
 // TypeScript:
+//export function get_char(): Parser<string | null> {
+  //return (state) => {
+    //var [state, skipped] = skip(state);
+    //if (state.index < state.code.length) {
+      //return [{...state, index: state.index + 1}, state.code[state.index]];
+    //} else {
+      //return [state, null];
+    //}
+  //}
+//}
+// Rust:
+fn get_char() -> Parser<char> {
+  return Box::new(move |state| {
+    let (state, skipped) = skip(state);
+    if state.index < state.code.len() {
+      return (State {code: state.code, index: state.index + 1}, state.code[state.index]);
+    } else {
+      return (state, '\0');
+    }
+  });
+}
+
+// TypeScript:
+//export const done : Parser<boolean> = (state) => {
+  //var [state, skipped] = skip(state);
+  //return [state, state.index === state.code.length];
+//};
+// Rust:
+fn done() -> Parser<bool> {
+  return Box::new(move |state| {
+    let (state, skipped) = skip(state);
+    return (state, state.index == state.code.len());
+  });
+}
+
+// TypeScript:
+//export function guard<A>(head: Parser<boolean>, body: Parser<A>) : Parser<A|null> {
+  //return (state) => {
+    //var [state, skipped] = skip(state);
+    //var [state, matched] = dry(head)(state);
+    //if (matched) {
+      //return body(state);
+    //} else {
+      //return [state, null];
+    //}
+  //};
+//}
+// Rust:
+fn guard<A: 'static>(head: Parser<bool>, body: Parser<A>) -> Parser<Option<A>> {
+  return Box::new(move |state| {
+    let (state, skipped) = skip(state);
+    let (state, matched) = dry(head)(state);
+    if matched {
+      let (state, got) = body(state);
+      return (state, Some(got));
+    } else {
+      return (state, None);
+    }
+  });
+}
+
+// TypeScript:
+//export function grammar<A>(name: string, choices: Array<Parser<A|null>>) : Parser<A> {
+  //return (state) => {
+    //for (var i = 0; i < choices.length; ++i) {
+      //var [state, got] = choices[i](state);
+      //if (got) {
+        //return [state, got];
+      //}
+    //}
+    //var fail : Parser<A> = expected_type(name);
+    //return fail(state);
+  //};
+//}
+// Rust:
+//fn grammar<A: 'static>(name: &'static str, choices: Vec<Parser<Option<A>>>) -> Parser<A> {
+  //return Box::new(move |state| {
+    //for i in 0..choices.len() {
+      //let (state, got) = choices[i](state);
+      //if got.is_some() {
+        //return (state, got.unwrap());
+      //}
+    //}
+    //return expected_type(name)(state);
+  //});
+//}
+
+// TypeScript:
+// export function dry<A>(parser: Parser<A>): Parser<A> {
+  //return (state) => {
+    //var [_, result] = parser(state);
+    //return [state, result];
+  //};
+//}
+// Rust:
+fn dry<A: 'static>(parser: Parser<A>) -> Parser<A> {
+  return Box::new(move |state| {
+    let (state, result) = parser(state);
+    return (state, result);
+  });
+}
+
+// TypeScript:
 //export function expected_string<A>(str: string): Parser<A> {
   //return (state) => {
     //throw "Expected '" + str + "':\n" + highlight(state.index, state.index + str.length, state.code);
@@ -173,3 +276,16 @@ fn expected_string<A>(c: &'static Code) -> Parser<A> {
     panic!("Expected '{}':\n{}", "TODO_CODE_TO_STRING", "TODO_HIGHLIGHT_FUNCTION");
   });
 }
+
+// TypeScript:
+//export function expected_type<A>(name: string): Parser<A> {
+  //return (state) => {
+    //throw "Expected " + name + ":\n" + highlight(state.index, state.index + 1, state.code);
+  //}
+//}
+// Rust:
+//fn expected_type<A>(name: &'static Code) -> Parser<A> {
+  //return Box::new(move |state| {
+    //panic!("Expected {}:\n{}", name, "TODO_HIGHLIGHT_FUNCTION");
+  //});
+//}
