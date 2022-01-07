dfe78bec78b0bbd1e70b746e3f93c42c78c59293
Rígille S. B. Menezes <rigillesbmenezes@protonmail.com>
2022-01-07 14:44:49 -0300

    test a simple parser

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@
 
 //use std::time::{SystemTime, UNIX_EPOCH};
 use std::time::Instant;
+use std::rc::Rc;
+use crate::text::Text;
+use crate::parser::*;
 
 mod lambolt;
 mod parser;
@@ -15,7 +18,31 @@ mod text;
 
 use runtime as rt;
 
+enum StrBinTree {
+  Tie{left: Box<StrBinTree>, right: Box<StrBinTree>},
+  Tip //{val: Text}
+}
+
+fn sbt<'a>(state: State) -> (State, bool) {
+    let text = Rc::new(text::utf8_to_text(
+        "()"
+      ));
+    let (state, either) = matchs(text)(state);
+    (state, either)
+}
 fn main() {
+  let state = parser::State {
+      code: &text::utf8_to_text(
+        "()"
+      ),
+      index: 0
+  };
+  let (new_state, result) = sbt(state);
+  if result {
+      println!("it's true, while the awful pray for tomorrow");
+  } else {
+      println!("it's false");
+  }
   //let mut worker = rt::new_worker();
   //worker.size = 1;
   //worker.node[0] = rt::Cal(0, 0, 0);
@@ -25,15 +52,15 @@ fn main() {
   //println!("* rwts: {} ({:.2}m rwt/s)", worker.cost, (worker.cost as f64) / total / 1000000.0);
   //println!("* time: {:?}", total);
 
-  println!(
-    "{}",
-    text::text_to_utf8(&text::highlight(
-      3,
-      7,
-      &text::utf8_to_text(
-        "oi tudo bem? como vai você hoje?\neu pessoalmente estou ok.\nespero que vc tbm"
-      )
-    ))
-  );
-  println!(":pp");
+//  println!(
+//    "{}",
+//    text::text_to_utf8(&text::highlight(
+//      3,
+//      7,
+//      &text::utf8_to_text(
+//        "oi tudo bem? como vai você hoje?\neu pessoalmente estou ok.\nespero que vc tbm"
+//      )
+//    ))
+//  );
+//  println!(":pp");
 }
