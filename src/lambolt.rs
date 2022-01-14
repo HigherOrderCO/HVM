ac240dbf590dd7e524b8c8148759ad7be8a2292c
rheidner <rheidner.achiles@gmail.com>
2022-01-14 14:59:53 -0300

    turn sanitize function deterministic

--- src/lambolt.rs
+++ src/lambolt.rs
@@ -1,4 +1,5 @@
 use crate::parser;
+use std::collections::BTreeMap;
 use std::fmt;
 use std::{collections::HashMap, fmt::format, u64};
 
@@ -434,9 +435,9 @@ pub fn sanitize(rule: &Rule) -> Result<SanitizeResult, String> {
   // Pass through the lhs of the function generating new names
   // for every variable found in the style described before with
   // the fresh function. Also checks if rule's left side is valid.
-  type NameTable = HashMap<String, String>;
+  type NameTable = BTreeMap<String, String>;
   fn create_fresh(rule: &Rule, fresh: &mut dyn FnMut() -> String) -> Result<NameTable, String> {
-    let mut table: HashMap<String, String> = HashMap::new();
+    let mut table = BTreeMap::new();
 
     let lhs = &rule.lhs;
     if let Term::Ctr { ref name, ref args } = **lhs {
@@ -476,7 +477,7 @@ pub fn sanitize(rule: &Rule) -> Result<SanitizeResult, String> {
   fn sanitize_term(
     term: &Term,
     lhs: bool,
-    tbl: &mut HashMap<String, String>,
+    tbl: &mut NameTable,
     ctx: &mut CtxSanitizeTerm,
   ) -> Result<Box<Term>, String> {
     let term = match term {
