be6ecf07e212d362ff4f3caf369ba99315a04bd8
MaiaVictor <srvictormaia@gmail.com>
2022-01-14 19:16:45 -0300

    Simplify interface of sanitize and gen_compilable

--- src/compilable.rs
+++ src/compilable.rs
@@ -1,38 +1,160 @@
 use std::collections::{BTreeMap, HashMap};
 use crate::lambolt as lb;
 
-// A Compilable is a Lambolt file ready for compilation.
-// It includes data such as:
+// Compilable
+// ==========
+
+// A Compilable is a Lambolt file ready for compilation. It includes:
 // - func_rules: sanitized rules grouped by function
-// - id_to_name: converts ctr/cal ids to names
-// - name_to_id: converts ctr/cal names to ids
+// - id_to_name: maps ctr ids to names
+// - name_to_id: maps ctr names to ids
 // - ctr_is_cal: true if a ctr is a cal
 // A sanitized rule has all its variables renamed to have unique names.
-// Variables that are never used are renamed to "" (empty string).
+// Variables that are never used are renamed to "*".
+
+pub struct Compilable {
+  pub func_rules: HashMap<String, Vec<lb::Rule>>,
+  pub id_to_name: HashMap<u64, String>,
+  pub name_to_id: HashMap<String, u64>,
+  pub ctr_is_cal: HashMap<String, bool>,
+}
+
+pub fn gen_compilable(file: &lb::File) -> Compilable {
+  // Generates a name table for a whole program. That table links constructor
+  // names (such as `cons` and `succ`) to small ids (such as `0` and `1`).
+  pub type NameToId = HashMap<String, u64>;
+  pub type IdToName = HashMap<u64, String>;
+  pub fn gen_name_to_id(rules: &Vec<lb::Rule>) -> NameToId {
+    fn find_ctrs(term: &lb::Term, table: &mut NameToId, fresh: &mut u64) {
+      match term {
+        lb::Term::Dup { expr, body, .. } => {
+          find_ctrs(expr, table, fresh);
+          find_ctrs(body, table, fresh);
+        }
+        lb::Term::Let { expr, body, .. } => {
+          find_ctrs(expr, table, fresh);
+          find_ctrs(body, table, fresh);
+        }
+        lb::Term::Lam { body, .. } => {
+          find_ctrs(body, table, fresh);
+        }
+        lb::Term::App { func, argm, .. } => {
+          find_ctrs(func, table, fresh);
+          find_ctrs(argm, table, fresh);
+        }
+        lb::Term::Op2 { val0, val1, .. } => {
+          find_ctrs(val0, table, fresh);
+          find_ctrs(val1, table, fresh);
+        }
+        lb::Term::Ctr { name, args } => {
+          let id = table.get(name);
+          if id.is_none() {
+            let first_char = name.chars().next();
+            if let Some(c) = first_char {
+              if c == '.' {
+                let id = &name[1..].parse::<u64>();
+                if let Ok(id) = id {
+                  table.insert(name.clone(), *id);
+                }
+              } else {
+                table.insert(name.clone(), *fresh);
+                *fresh += 1;
+              }
+            }
+            for arg in args {
+              find_ctrs(arg, table, fresh);
+            }
+          }
+        }
+        _ => (),
+      }
+    }
+    let mut table = HashMap::new();
+    let mut fresh = 0;
+    for rule in rules {
+      find_ctrs(&rule.lhs, &mut table, &mut fresh);
+      find_ctrs(&rule.rhs, &mut table, &mut fresh);
+    }
+    table
+  }
+  pub fn invert(name_to_id: &NameToId) -> IdToName {
+    let mut id_to_name: IdToName = HashMap::new();
+    for (name, id) in name_to_id {
+      id_to_name.insert(*id, name.clone());
+    }
+    return id_to_name;
+  }
+
+  // Finds constructors that are used as functions.
+  pub type IsFunctionTable = HashMap<String, bool>;
+  pub fn gen_ctr_is_cal(rules: &Vec<lb::Rule>) -> IsFunctionTable {
+    let mut is_call: IsFunctionTable = HashMap::new();
+    for rule in rules {
+      let term = &rule.lhs;
+      if let lb::Term::Ctr { ref name, .. } = **term {
+        // FIXME: this looks wrong, will check later
+        is_call.insert(name.clone(), true);
+      }
+    }
+    is_call
+  }
+
+  // Groups rules by name. For example:
+  //   (add (succ a) (succ b)) = (succ (succ (add a b)))
+  //   (add (succ a) (zero)  ) = (succ a)
+  //   (add (zero)   (succ b)) = (succ b)
+  //   (add (zero)   (zero)  ) = (zero)
+  // This is a group of 4 rules starting with the "add" name.
+  pub type FuncRules = HashMap<String, Vec<lb::Rule>>;
+  pub fn gen_func_rules(rules: &Vec<lb::Rule>) -> FuncRules {
+    let mut groups: FuncRules = HashMap::new();
+    for rule in rules {
+      if let lb::Term::Ctr { ref name, ref args } = *rule.lhs {
+        let group = groups.get_mut(name);
+        let rule = sanitize_rule(&rule).unwrap();
+        match group {
+          None => {
+            groups.insert(name.clone(), Vec::from([rule]));
+          }
+          Some(group) => {
+            group.push(rule);
+          }
+        }
+      }
+    }
+    groups
+  }
+
+  let func_rules = gen_func_rules(&file.rules);
+  let name_to_id = gen_name_to_id(&file.rules);
+  let id_to_name = invert(&name_to_id);
+  let ctr_is_cal = gen_ctr_is_cal(&file.rules);
+  return Compilable {
+    func_rules,
+    name_to_id,
+    id_to_name,
+    ctr_is_cal,
+  };
+}
+
+// Sanitize
+// ========
 
 pub struct SanitizedRule {
   pub rule: lb::Rule,
   pub uses: HashMap<String, u64>,
 }
 
-pub struct Compilable {
-  pub func_rules: HashMap<String, Vec<SanitizedRule>>,
-  pub id_to_name: HashMap<u64, String>,
-  pub name_to_id: HashMap<String, u64>,
-  pub ctr_is_cal: HashMap<String, bool>,
-}
-
-// Sanitize
-// ========
-
-// This big function sanitizes a rule. That is, it renames every variable in a
-// rule, in order to make it unique. Moreover, it will also add a `.N` to the
-// end of the name of each variable used in the right-hand side of the rule,
-// where `N` stands for the number of times it was used. For example:
-//   sanitize `(fn (cons head tail)) = (cons (pair head head) tail)`
-//         ~> `(fn (cons x0   x1))   = (cons (pair x0.0 x0.1) x1.0)`
-// It also returns the usage count of each variable.
-pub fn sanitize_rule(rule: &lb::Rule) -> Result<SanitizedRule, String> {
+// This big function sanitizes a rule. That has the following effect:
+// - All variables are renamed to have a global unique name.
+// - All variables are linearized.
+//   - If they're used more than once, dups are inserted.
+//   - If they're used once, nothing changes.
+//   - If they're never used, their name is changed to "*".
+// Example:
+//   - sanitizing: `(Foo a b) = (+ a a)`
+//   - results in: `(Foo x0 *) = dup x0.0 x0.1 = x0; (+ x0.0 x0.1)`
+pub fn sanitize_rule(rule: &lb::Rule) -> Result<lb::Rule, String> {
   // Pass through the lhs of the function generating new names
   // for every variable found in the style described before with
   // the fresh function. Also checks if rule's left side is valid.
@@ -194,6 +316,26 @@ pub fn sanitize_rule(rule: &lb::Rule) -> Result<SanitizedRule, String> {
     Ok(term)
   }
 
+  // Renames unused variables to "*"
+  fn rename_unused(term: &mut lb::Term, uses: &HashMap<String, u64>) {
+    match term {
+      lb::Term::Var { name } => {
+        if let Some(x) = uses.get(name) {
+          if *x == 0 {
+            *name = "*".to_string();
+          }
+        }
+      }
+      lb::Term::Dup { expr, body, nam0, nam1, } => {}
+      lb::Term::Let { name, expr, body } => {}
+      lb::Term::Lam { name, body } => {}
+      lb::Term::App { func, argm } => {}
+      lb::Term::Ctr { name, args } => {}
+      lb::Term::Op2 { oper, val0, val1 } => {}
+      lb::Term::U32 { numb } => {}
+    }
+  }
+
   // Duplicates all variables that are used more than once.
   // The process is done generating auxiliary variables and
   // applying dup on them.
@@ -290,141 +432,21 @@ pub fn sanitize_rule(rule: &lb::Rule) -> Result<SanitizedRule, String> {
     fresh: &mut fresh,
   };
 
-  // sanitize left side
-  let lhs = sanitize_term(&rule.lhs, true, &mut table.clone(), &mut ctx)?;
-  // sanitize right side
+  // sanitize left and right sides
+  let mut lhs = sanitize_term(&rule.lhs, true, &mut table.clone(), &mut ctx)?;
   let mut rhs = sanitize_term(&rule.rhs, false, &mut table.clone(), &mut ctx)?;
 
   // duplicate right side variables that are used more than once
   for (key, value) in table {
-    let expr = Box::new(lb::Term::Var {
-      name: value.clone(),
-    });
+    let expr = Box::new(lb::Term::Var { name: value.clone() });
     rhs = duplicator(&value, expr, rhs, &mut uses);
   }
 
-  // forms the new rules
-  let rule = lb::Rule { lhs, rhs };
-  Ok(SanitizedRule { rule, uses })
+  // renames unused variables to "*"
+  rename_unused(&mut lhs, &uses);
+  rename_unused(&mut rhs, &uses);
+
+  // returns the sanitized rule
+  Ok(lb::Rule { lhs, rhs })
 }
 
-// Compilable
-// ==========
-
-pub fn gen_compilable(file: &lb::File) -> Compilable {
-  // Generates a name table for a whole program. That table links constructor
-  // names (such as `cons` and `succ`) to small ids (such as `0` and `1`).
-  pub type NameToId = HashMap<String, u64>;
-  pub type IdToName = HashMap<u64, String>;
-  pub fn gen_name_to_id(rules: &Vec<lb::Rule>) -> NameToId {
-    fn find_ctrs(term: &lb::Term, table: &mut NameToId, fresh: &mut u64) {
-      match term {
-        lb::Term::Dup { expr, body, .. } => {
-          find_ctrs(expr, table, fresh);
-          find_ctrs(body, table, fresh);
-        }
-        lb::Term::Let { expr, body, .. } => {
-          find_ctrs(expr, table, fresh);
-          find_ctrs(body, table, fresh);
-        }
-        lb::Term::Lam { body, .. } => {
-          find_ctrs(body, table, fresh);
-        }
-        lb::Term::App { func, argm, .. } => {
-          find_ctrs(func, table, fresh);
-          find_ctrs(argm, table, fresh);
-        }
-        lb::Term::Op2 { val0, val1, .. } => {
-          find_ctrs(val0, table, fresh);
-          find_ctrs(val1, table, fresh);
-        }
-        lb::Term::Ctr { name, args } => {
-          let id = table.get(name);
-          if id.is_none() {
-            let first_char = name.chars().next();
-            if let Some(c) = first_char {
-              if c == '.' {
-                let id = &name[1..].parse::<u64>();
-                if let Ok(id) = id {
-                  table.insert(name.clone(), *id);
-                }
-              } else {
-                table.insert(name.clone(), *fresh);
-                *fresh += 1;
-              }
-            }
-            for arg in args {
-              find_ctrs(arg, table, fresh);
-            }
-          }
-        }
-        _ => (),
-      }
-    }
-    let mut table = HashMap::new();
-    let mut fresh = 0;
-    for rule in rules {
-      find_ctrs(&rule.lhs, &mut table, &mut fresh);
-      find_ctrs(&rule.rhs, &mut table, &mut fresh);
-    }
-    table
-  }
-  pub fn invert(name_to_id: &NameToId) -> IdToName {
-    let mut id_to_name: IdToName = HashMap::new();
-    for (name, id) in name_to_id {
-      id_to_name.insert(*id, name.clone());
-    }
-    return id_to_name;
-  }
-
-  // Finds constructors that are used as functions.
-  pub type IsFunctionTable = HashMap<String, bool>;
-  pub fn gen_ctr_is_cal(rules: &Vec<lb::Rule>) -> IsFunctionTable {
-    let mut is_call: IsFunctionTable = HashMap::new();
-    for rule in rules {
-      let term = &rule.lhs;
-      if let lb::Term::Ctr { ref name, .. } = **term {
-        // FIXME: this looks wrong, will check later
-        is_call.insert(name.clone(), true);
-      }
-    }
-    is_call
-  }
-
-  // Groups rules by name. For example:
-  //   (add (succ a) (succ b)) = (succ (succ (add a b)))
-  //   (add (succ a) (zero)  ) = (succ a)
-  //   (add (zero)   (succ b)) = (succ b)
-  //   (add (zero)   (zero)  ) = (zero)
-  // This is a group of 4 rules starting with the "add" name.
-  pub type FuncRules = HashMap<String, Vec<SanitizedRule>>;
-  pub fn gen_func_rules(rules: &Vec<lb::Rule>) -> FuncRules {
-    let mut groups: FuncRules = HashMap::new();
-    for rule in rules {
-      if let lb::Term::Ctr { ref name, ref args } = *rule.lhs {
-        let group = groups.get_mut(name);
-        let sanit = sanitize_rule(&rule).unwrap();
-        match group {
-          None => {
-            groups.insert(name.clone(), Vec::from([sanit]));
-          }
-          Some(group) => {
-            group.push(sanit);
-          }
-        }
-      }
-    }
-    groups
-  }
-
-  let func_rules = gen_func_rules(&file.rules);
-  let name_to_id = gen_name_to_id(&file.rules);
-  let id_to_name = invert(&name_to_id);
-  let ctr_is_cal = gen_ctr_is_cal(&file.rules);
-  return Compilable {
-    func_rules,
-    name_to_id,
-    id_to_name,
-    ctr_is_cal,
-  };
-}
