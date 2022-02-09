c230b2bee6889211f32ce1490cb1991f48d3c6ad
Kelvin Santos <kelvinsteinersantos@gmail.com>
2022-02-09 15:38:50 -0300

    print program stats to stderr instead

--- src/runtime.c
+++ src/runtime.c
@@ -1372,9 +1372,9 @@ int main(int argc, char* argv[]) {
   // Prints result statistics
   u64 delta_time = (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec;
   double rwt_per_sec = (double)ffi_cost / (double)delta_time;
-  printf("Rewrites: %"PRIu64" (%.2f MR/s).\n", ffi_cost, rwt_per_sec);
-  printf("Mem.Size: %"PRIu64" words.\n", ffi_size);
-  printf("\n");
+  fprintf(stderr, "Rewrites: %"PRIu64" (%.2f MR/s).\n", ffi_cost, rwt_per_sec);
+  fprintf(stderr, "Mem.Size: %"PRIu64" words.\n", ffi_size);
+  fprintf(stderr, "\n");
 
   // Prints result normal form
   const u64 code_mcap = 256 * 256 * 256; // max code size = 16 MB
